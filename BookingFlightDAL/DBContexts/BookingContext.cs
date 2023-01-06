a9078b49acf36ab276425cc0b382033569c709bc
yadidro <ROY785@GMAIL.COM>
2023-01-06 21:12:31 +0200

    order projects

---
+++ BookingFlightDAL/DBContexts/BookingContext.cs
@@ -0,0 +1,28 @@
+﻿using BookingFlightService.BookingFlightDAL.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace BookingFlightService.BookingFlightDAL.DBContexts
+{
+    public class BookingContext : DbContext
+    {
+        public DbSet<Booking> Bookings { get; set; }
+
+        private readonly IConfiguration _configuration;
+
+        public BookingContext(DbContextOptions<BookingContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DatabaseConnection"));
+            }
+        }
+    }
+}
